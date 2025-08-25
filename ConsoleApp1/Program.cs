97e7afa79bb85ceeb10dab54a2bf98afa1cdef79
Anton <anton@bluejay.be>
2025-08-25 18:08:20 +0200

    move variables

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,17 +2,16 @@
 
 internal class Program {
     private const string FilePath = "./assets/input.txt";
+    private const int TargetLength = 6;
+    private const int MaxParts = 3;
 
     private static void Main(string[] args) {
         var words = FileReader.GetLines(FilePath);
         var repository = new HashSet<string>(words);
 
-        var targetLength = 6;
-        var maxParts = 3;
-
-        var targetWords = words.Where(w => w.Length == targetLength);
+        var targetWords = words.Where(w => w.Length == TargetLength);
         foreach (var word in targetWords) {
-            var combinations = CombinationFinder.FindCombinations(word, repository, maxParts);
+            var combinations = CombinationFinder.FindCombinations(word, repository, MaxParts);
 
             foreach (var combination in combinations) {
                 Console.WriteLine($"{string.Join('+', combination.Parts)}={combination.Word}");
