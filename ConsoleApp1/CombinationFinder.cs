544562fd9c2d614fb1c3bcc44c2af0569ded55b0
Anton <anton@bluejay.be>
2025-08-25 17:58:29 +0200

    use yield in combinationfinder

--- ConsoleApp1/CombinationFinder.cs
+++ ConsoleApp1/CombinationFinder.cs
@@ -1,17 +1,13 @@
 namespace ConsoleApp1;
 
 public static class CombinationFinder {
-    public static List<Combination> FindCombinations(string word, HashSet<string> repository, int maxParts) {
-        var results = new List<Combination>();
-
+    public static IEnumerable<Combination> FindCombinations(string word, HashSet<string> repository, int maxParts) {
         for (var parts = 2; parts <= maxParts; parts++) {
             foreach (var splitParts in WordSplitter.SplitWord(word, parts)) {
                 if (splitParts.All(repository.Contains)) {
-                    results.Add(new Combination(splitParts, word));
+                    yield return new Combination(splitParts, word);
                 }
             }
         }
-
-        return results;
     }
 }
