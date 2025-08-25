baf311ba4c0f59fefcd27f2914c0f6a9bd0eff60
Anton <anton@bluejay.be>
2025-08-25 18:11:17 +0200

    use Where instead of if

--- ConsoleApp1/CombinationFinder.cs
+++ ConsoleApp1/CombinationFinder.cs
@@ -3,10 +3,10 @@ namespace ConsoleApp1;
 public static class CombinationFinder {
     public static IEnumerable<Combination> FindCombinations(string word, HashSet<string> repository, int maxParts) {
         for (var parts = 2; parts <= maxParts; parts++) {
-            foreach (var splitParts in WordSplitter.SplitWord(word, parts)) {
-                if (splitParts.All(repository.Contains)) {
-                    yield return new Combination(splitParts, word);
-                }
+            var validSplits = WordSplitter.SplitWord(word, parts).Where(s => s.All(repository.Contains));
+
+            foreach (var validSplitParts in validSplits) {
+                yield return new Combination(validSplitParts, word);
             }
         }
     }
